0b802fd0a6ca17232dc42c5f8daa716d02b16a33
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-01-25 20:33:47 +0000

    Add uikit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule, MatGridListModule, MatInputModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
