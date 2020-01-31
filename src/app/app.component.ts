146b3cc2461d7c987e9495b07caa349616b3e915
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-01-31 08:44:42 +0000

    Remove unused ChangeDetectionStrategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {BookStoreService} from './book-store.service';
 import {Book} from './book';
 
