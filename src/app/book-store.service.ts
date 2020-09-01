1c3bac39e6c8ab2aeb6621f9dcb86762101fa94c
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-09-01 22:53:20 +0100

    Add SSE service

--- src/app/book-store.service.ts
+++ src/app/book-store.service.ts
@@ -8,14 +8,14 @@ export class BookStoreService {
 
   constructor(private httpClient: HttpClient) { }
 
-  private url = 'http://localhost:8080/books';
+  private url = 'http://localhost:8080/books/reactive';
   private httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type':  'application/json'
+      Accept: 'text/event-stream'
     })
   };
 
-  public getBooks(): Observable<Book[]> {
-    return this.httpClient.get<Book[]>(this.url, this.httpOptions);
+  public getBooks(): Observable<Book> {
+    return this.httpClient.get<Book>(this.url, this.httpOptions);
   }
 }
